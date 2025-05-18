d77c0b21cc4b35238e7e1a764759e1039db8f5be
Joss Lei <josslei@163.com>
2025-05-18 20:34:22 +0800

    data(GSO): fix generation script bugs - 2
    Prepare data based on models given by Google Scanned Objects (GSO).

--- datasets/generate_dataset.py
+++ datasets/generate_dataset.py
@@ -27,7 +27,9 @@ def random_pose(table_z: float = 0.75) -> Tuple[np.ndarray, np.ndarray]:
     return np.asarray([x, y, z]), np.asarray(quat)
 
 
-def make_random_object_loader(bc: BulletClient, obj_dir: pathlib.Path) -> Callable[[], int]:
+def make_random_object_loader(
+    bc: BulletClient, obj_dir: pathlib.Path
+) -> Callable[[], int]:
     dir_list: List[pathlib.Path] = [d for d in obj_dir.iterdir() if d.is_dir()]
     urdf_path_list: List[pathlib.Path] = [
         d / "model.urdf" for d in dir_list if (d / "model.urdf").exists()
@@ -49,42 +51,41 @@ def make_random_object_loader(bc: BulletClient, obj_dir: pathlib.Path) -> Callab
 
 
 def setup_simulation(bc: BulletClient) -> int:
-    """Reset the simulation environment and load the table and plane.
+    """Reset the simulation environment and load the plane.
 
     Returns:
-        int: The ID of the table object
+        int: The ID of the plane object
     """
     bc.resetSimulation()
     bc.setGravity(0, 0, -9.81)
     bc.setAdditionalSearchPath(pybullet_data.getDataPath())
-    table_id: int = bc.loadURDF("table/table.urdf", [0, 0, 0.7])
-    _ = bc.loadURDF("plane.urdf")
-    return table_id
+    plane_id: int = bc.loadURDF("plane.urdf")
+    return plane_id
 
 
 def render_pointcloud(
     bc: BulletClient, width: int = 640, height: int = 480
 ) -> Tuple[np.ndarray, np.ndarray]:
     view_matrix: List[float] = bc.computeViewMatrix(
-        # cameraEyePosition=[0.5, 0, 1.2],
-        cameraEyePosition=[0, 0, 2],
-        # cameraTargetPosition=[0, 0, 0.7],
+        cameraEyePosition=[0, 0, 1],
         cameraTargetPosition=[0, 0, 0],
-        cameraUpVector=[0, 0, 1],
+        cameraUpVector=[0, 1, 0],
     )
     projection_matrix: List[float] = bc.computeProjectionMatrixFOV(
-        fov=60, aspect=width / height, nearVal=0.1, farVal=2.0
+        fov=60, aspect=width / height, nearVal=0.1, farVal=5.0
     )
     img = bc.getCameraImage(
         width,
         height,
         view_matrix,
         projection_matrix,
-        renderer=bc.ER_BULLET_HARDWARE_OPENGL,
-        flags=bc.ER_SEGMENTATION_MASK_OBJECT_AND_LINKINDEX,
+        renderer=bc.ER_TINY_RENDERER,
     )
+    import cv2
+
+    cv2.imwrite("tmp.png", img[2])
     depth_buffer: np.ndarray = (
-        np.array(img[3]).astype(np.float32) / 1000.0
+        np.array(img[3]).astype(np.float32) * 1000.0
     )  # Convert from mm to meters
     segmentation_mask: np.ndarray = np.array(img[4]).astype(np.int32)
 
@@ -97,12 +98,15 @@ def render_pointcloud(
     points: np.ndarray = np.dstack((xs, -ys, -zs)).reshape(-1, 3)
 
     # Extract object IDs
-    object_ids: np.ndarray = segmentation_mask.reshape(-1) >> 24
+    object_ids: np.ndarray = segmentation_mask.reshape(-1)
     return points, object_ids
 
 
 def save_scene(
-    out_dir: pathlib.Path, scene_idx: int, language: str, obj_pointclouds: Dict[int, np.ndarray]
+    out_dir: pathlib.Path,
+    scene_idx: int,
+    language: str,
+    obj_pointclouds: Dict[int, np.ndarray],
 ) -> None:
     scene_dir: pathlib.Path = out_dir / f"{scene_idx:06d}"
     scene_dir.mkdir(parents=True, exist_ok=True)
@@ -133,9 +137,12 @@ class GenerateSceneWorker:
 
     def __call__(self, scene_idx: int) -> int:
         bc = BulletClient(connection_mode=pb.DIRECT)
-        setup_simulation(bc)
-        _load_random_object: Callable[[], int] = make_random_object_loader(bc, self.obj_dir)
-        obj_ids: List[int] = [
+        plane_id: int = setup_simulation(bc)
+        assert plane_id == 0
+        _load_random_object: Callable[[], int] = make_random_object_loader(
+            bc, self.obj_dir
+        )
+        obj_ids: List[int] = [plane_id] + [
             _load_random_object() for _ in range(random.randint(*self.obj_count_range))
         ]
 
@@ -145,7 +152,9 @@ class GenerateSceneWorker:
 
         points, obj_mask = render_pointcloud(bc)
         obj_clouds: Dict[int, np.ndarray] = {
-            oid: points[obj_mask == oid] for oid in np.unique(obj_mask) if oid in obj_ids
+            oid: points[obj_mask == oid]
+            for oid in np.unique(obj_mask)
+            if oid in obj_ids
         }
 
         language_instruction: str = "Place a small circle in the middle of the table."
@@ -171,4 +180,4 @@ def main(
 
 
 if __name__ == "__main__":
-    main(pathlib.Path("GSO_extracted"), pathlib.Path("scenes_GSO"), 1)
+    main(pathlib.Path("GSO/models"), pathlib.Path("GSO/scenes"), 1)
